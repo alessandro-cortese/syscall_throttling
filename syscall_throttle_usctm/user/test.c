450aee15eb61c702aaccbf7d566b4beb7a370c6e
alessandro-cortese <ale.cortese99@gmail.com>
2026-03-25 09:31:54 +0100

    After report to prof
    Create simple test

---
+++ syscall_throttle_usctm/user/test.c
@@ -0,0 +1,14 @@
+// tester_openat.c
+#define _GNU_SOURCE
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <stdio.h>
+
+int main(int a, char **b) {
+
+    printf("francesco\n");
+    fflush(stdout);
+
+    return 0;
+}
